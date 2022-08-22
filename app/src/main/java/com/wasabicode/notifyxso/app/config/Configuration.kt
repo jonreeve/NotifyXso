07ffe43da75218a611cac6c22492f56e1630b648
Jon Reeve <jonreeve@users.noreply.github.com>
2022-08-22 12:42:45 +0100

    Add a Configuration data class

--- app/src/main/java/com/wasabicode/notifyxso/app/config/Configuration.kt
+++ app/src/main/java/com/wasabicode/notifyxso/app/config/Configuration.kt
@@ -11,25 +11,35 @@ interface Configuration {
     var preferredIcon: PreferredIcon
 }
 
+data class ConfigurationVO(
+    override var enabled: Boolean = false,
+    override var host: String = "192.168.1.",
+    override var port: Int = 43210,
+    override var durationSecs: Float = 2f,
+    override var exclusions: Set<String> = defaultExclusions,
+    override var preferredIcon: PreferredIcon = PreferredIcon.Custom
+) : Configuration
+
+private val defaultExclusions = setOf(
+    "Pebble Time",
+    "Checking for new messages",
+    "Checking for messages…",
+    "Loading...",
+    "USB debugging connected",
+    "Charging this device via USB",
+    "On sale from your wishlist",
+)
+private val Defaults = ConfigurationVO()
+
 class SharedPrefsConfiguration(context: Context) : Configuration {
     private val sharedPrefs = context.getSharedPreferences("config", Context.MODE_PRIVATE)
 
     override var enabled by SharedPrefsDelegate.boolean(sharedPrefs)
-    override var host by SharedPrefsDelegate.string(sharedPrefs, defaultValue = "192.168.1.")
-    override var port by SharedPrefsDelegate.int(sharedPrefs, defaultValue = 43210)
-    override var durationSecs by SharedPrefsDelegate.float(sharedPrefs, defaultValue = 2.0f)
-    override var exclusions by SharedPrefsDelegate.stringSet(
-        sharedPrefs, defaultValue = setOf(
-            "Pebble Time",
-            "Checking for new messages",
-            "Checking for messages…",
-            "Loading...",
-            "USB debugging connected",
-            "Charging this device via USB",
-            "On sale from your wishlist",
-        )
-    )
-    override var preferredIcon: PreferredIcon by SharedPrefsDelegate.int(sharedPrefs, defaultValue = PreferredIcon.Custom.ordinal)
+    override var host by SharedPrefsDelegate.string(sharedPrefs, defaultValue = Defaults.host)
+    override var port by SharedPrefsDelegate.int(sharedPrefs, defaultValue = Defaults.port)
+    override var durationSecs by SharedPrefsDelegate.float(sharedPrefs, defaultValue = Defaults.durationSecs)
+    override var exclusions by SharedPrefsDelegate.stringSet(sharedPrefs, defaultValue = Defaults.exclusions)
+    override var preferredIcon: PreferredIcon by SharedPrefsDelegate.int(sharedPrefs, defaultValue = Defaults.preferredIcon.ordinal)
         .mapped(
             get = { PreferredIcon.values()[it] },
             set = { it.ordinal }
