7e7427a11b9a7a7a8e91bd34515f4acd2a73ffad
Jon Reeve <jonreeve@users.noreply.github.com>
2022-09-22 17:53:20 +0100

    Drop startup args functionality for now
    I'm never using it, and it's the only reason I'm still creating the VM
    where I am instead of from the Composable. Can figure out how to do
    that again later but since I never use it for now I can ditch it.

--- app/src/main/java/com/wasabicode/notifyxso/app/MainActivity.kt
+++ app/src/main/java/com/wasabicode/notifyxso/app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.wasabicode.notifyxso.app.MainViewModel.Intention.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,26 +11,10 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: MainViewModel by viewModels()
 
-    private val enableOnStartArg by lazy { intent.getBooleanExtra(EXTRA_ENABLE_ON_START, false) }
-    private val hostArg by lazy { intent.getStringExtra(EXTRA_HOST) }
-    private val portArg by lazy { intent.getIntExtra(EXTRA_PORT, -1).takeIf { it >= 0 } }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        processArgs()
         setContent {
             MainScreen(viewModel = viewModel)
         }
-    }
-
-    private fun processArgs() {
-        viewModel.act(HandleStartArguments(hostArg, portArg, enableOnStartArg))
-    }
-
-    companion object {
-        const val EXTRA_ENABLE_ON_START = "on"
-        const val EXTRA_HOST = "host"
-        const val EXTRA_PORT = "port"
     }
 }
