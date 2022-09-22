5d303441aa5cecdeb52c93de4dcaa8a09c75eb8a
Jon Reeve <jonreeve@users.noreply.github.com>
2022-09-22 17:53:40 +0100

    Create the VM from Composable

--- app/src/main/java/com/wasabicode/notifyxso/app/MainActivity.kt
+++ app/src/main/java/com/wasabicode/notifyxso/app/MainActivity.kt
@@ -3,18 +3,14 @@ package com.wasabicode.notifyxso.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen(viewModel = viewModel)
+            MainScreen()
         }
     }
 }
